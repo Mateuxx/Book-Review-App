1dc9c2e39571ec6feea0a75ca8f79902bc6b9c36
guilhermesilveiraa <guilherme.silveira@icomp.ufam.edu.br>
2024-09-04 14:51:07 -0400

    Tela de Home - XML - Recycler view Horizontal Adapter Home

---
+++ BookAppReview/app/src/main/java/com/example/bookappreview/ui/viewModel/HomeViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.bookappreview.ui.viewModel
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.room.Query
+import com.example.bookappreview.model.Livro
+import com.example.bookappreview.repository.MainRepository
+import kotlinx.coroutines.launch
+
+class HomeViewModel(
+    private val repository: MainRepository
+) : ViewModel(){
+
+    private val _livros = MutableLiveData<List<Livro>>()
+    val livros: LiveData<List<Livro>> get() = _livros
+
+    fun fetchBooks(searchQuery : String, context: Context) {
+        viewModelScope.launch {
+            val books = repository.fetchBooks(searchQuery,context)
+            _livros.postValue(books)
+        }
+    }
+}
