f3529a590e625d653841007507e42a8ae4521464
Mateus Mota Nobrega-MNOBRE <mnobre@motorola.com>
2024-10-18 15:11:55 -0400

    some codesFixes

--- BookAppReview - Clean + MVVM+ Compose/app/src/main/java/com/example/bookappreview/presentation/screens/BookDetalisScreen.kt
+++ BookAppReview - Clean + MVVM+ Compose/app/src/main/java/com/example/bookappreview/presentation/screens/BookDetalisScreen.kt
@@ -1,7 +1,6 @@
 package com.example.bookappreview.presentation.screens
 
 import android.util.Log
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -19,10 +18,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import coil.compose.AsyncImage
 import com.example.bookappreview.R
 import com.example.bookappreview.presentation.components.MyDivider
 import com.example.bookappreview.presentation.model.LivroParcelable
@@ -36,7 +39,7 @@ fun BookDetailsScreen(
     // Observa o livro selecionado
     val selectedBook by viewModel.selectedBook.collectAsState()
     Log.i("TAG", "BookDetailsScreen: Livro no sharedViewmodel = $selectedBook")
-//    BookDetailsContent(book = selectedBook!!)
+    BookDetailsContent(book = selectedBook!!)
 }
 
 @Composable
@@ -58,37 +61,40 @@ fun BookDetailsContent(book: LivroParcelable) {
             ) {
                 // Infos - título, autor, ano
                 Text(
-                    text = "uiState.title",
+                    text = book.title!!,
                     style = MaterialTheme.typography.h5,
                     color = Color.White,
                     fontWeight = FontWeight.Bold
                 )
 
-                Spacer(modifier = Modifier.height(4.dp))
+
+                Spacer(modifier = Modifier.height(24.dp))
 
                 Text(
-                    text = "• Written by ",
+                    text = "${book.year!!} • Written by ",
                     style = MaterialTheme.typography.body2,
                     color = Color.Gray
                 )
+
                 Text(
-                    text = "uiState.title",
-                    style = MaterialTheme.typography.h6,
+                    text = book.autor!!,
+                    style = MaterialTheme.typography.body1,
                     color = Color.Gray,
                     fontWeight = FontWeight.Bold
                 )
 
-                Spacer(modifier = Modifier.height(100.dp))
+                Spacer(modifier = Modifier.height(90.dp))
             }
 
             // Capa do livro
-            Image(
-                painter = painterResource(id = R.drawable.ic_launcher_background), // Substitua com a imagem correta
-                contentDescription = null,
+            AsyncImage(
+                model = book.imagem,
+                contentDescription =null,
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
                     .height(160.dp)
-                    .width(120.dp)
+                    .width(120.dp),
+                placeholder = painterResource(id = R.drawable.imagem_padrao)
             )
         }
 
@@ -96,7 +102,9 @@ fun BookDetailsContent(book: LivroParcelable) {
         Text(
             text = book.description!!,
             style = MaterialTheme.typography.body1,
-            color = Color.Gray
+            color = Color.Gray,
+            maxLines = 8,
+            overflow = TextOverflow.Ellipsis
         )
 
         Spacer(modifier = Modifier.height(14.dp))
@@ -106,12 +114,11 @@ fun BookDetailsContent(book: LivroParcelable) {
 }
 
 
-//@Preview(showBackground = true)
-//@Composable
-//private fun BookDetailsScreenPreview() {
-//    BookDetailsScreen(
-//        navController = NavHostController(LocalContext.current),
-//        detailsViewModel = BookDetalisScreensViewModel(),
-//        sharedViewModel = BookSharedViewModel()
-//    )
-//}
+@Preview(showBackground = true)
+@Composable
+private fun BookDetailsScreenPreview() {
+    BookDetailsScreen(
+        navController = NavHostController(LocalContext.current),
+        viewModel = BookSharedViewModel()
+    )
+}
